78a7f407dd4044b3753e2f5f7ad089eb3a5269fd
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2024-07-28 15:30:39 +0200

    feat: Add state to search input

--- src/component/TodoSearch.js
+++ src/component/TodoSearch.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import "../css/TodoSearch.css";
 
 export function TodoSearch() {
+  const [searchValue, setSearchValue] = useState("");
+  console.log(searchValue);
   return (
     <div className="todoSearch">
-      <input placeholder="Sacar al perro a pasear" />
+      <input
+        placeholder="Sacar al perro a pasear"
+        value={searchValue}
+        onChange={(event) => {
+          const newSearchValue = event.target.value;
+          setSearchValue(newSearchValue);
+        }}
+      />
     </div>
   );
 }
