ff61074d7df9b1ca8151658db205e8a7ab54dbc9
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2024-07-28 15:44:41 +0200

    feat: Fix accesibility errors in button 'CreateTodoButton'

--- src/component/CreateTodoButton.js
+++ src/component/CreateTodoButton.js
@@ -3,6 +3,8 @@ import "../css/CreateTodoButton.css";
 export function CreateTodoButton() {
   return (
     <button
+      title="Crear nueva tarea"
+      type="button"
       className="createTodoButton"
       onClick={(event) => {
         console.log("Le diste al boton de añadir todos");
