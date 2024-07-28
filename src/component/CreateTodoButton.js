2c4b24aa4bb03f90eef72970d381beb03841da4d
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2024-07-28 15:32:24 +0200

    feat: Add event onclick in component 'CreateTodoButton'

--- src/component/CreateTodoButton.js
+++ src/component/CreateTodoButton.js
@@ -1,5 +1,14 @@
 import "../css/CreateTodoButton.css";
 
 export function CreateTodoButton() {
-  return <button className="createTodoButton"></button>;
+  return (
+    <button
+      className="createTodoButton"
+      onClick={(event) => {
+        console.log("Le diste al boton de añadir todos");
+        console.log(event);
+        console.log(event.target);
+      }}
+    ></button>
+  );
 }
