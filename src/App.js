785ca65ef6d0fad1545da2b9f432f240e432906b
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2024-08-16 20:02:25 +0200

    feat: Clean code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import { ApiUI } from "./AppUI";
 import { TodoPorvider } from "./components/TodoContext";
 
-/* const defaultTodos = [
-  { text: "Cortar cebolla", completed: true },
-  { text: "Tomar el Curso de Intro a React.js", completed: false },
-  { text: "Llorar con la Llorona", completed: false },
-  { text: "Comprar pan", completed: false },
-  { text: "Lavar ropa", completed: true },
-];
-const stringifyTodos = JSON.stringify(defaultTodos)
-localStorage.setItem('TODOS_V1', stringifyTodos) */
-// localStorage.removeItem('TODOS_V1')
-
 function App() {
   
   return (
